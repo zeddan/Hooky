f2cf606829a4f6b52e004c6c0b0bb6de170f19c3
Madeleine <maddepersoon@gmail.com>
2017-04-06 15:58:40 +0200

    add get/post for products

--- server/app.py
+++ server/app.py
@@ -1,7 +1,74 @@
-from flask import Flask
+from flask import Flask, jsonify, abort, request
+from flask_sqlalchemy import SQLAlchemy
+import json
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    email = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
+
+    def __init__(self, name, email, admin):
+        self.name = name
+        self.email = email
+        self.admin = admin
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    image = db.Column(db.String(80))
+    description = db.Column(db.String(200))
+    published = db.Column(db.Boolean)
+
+    def __init__(self, name, image, description, published):
+        self.name = name
+        self.image = image
+        self.description = description
+        self.published = published
+
+    def serialize(self):
+        return {
+                'id':self.id,
+                'name':self.name,
+                'image':self.image,
+                'description':self.description,
+                'published':self.published,
+        }
+
+db.create_all()
+
+@app.route('/users/', methods=['GET', 'POST'])
+def users():
+    if request.method == 'GET':
+        return jsonify({'users':User.query.all()})
+
+@app.route('/users/<int:id>/')
+def get_user(id):
+    return jsonify({'user':User.query.get(id)})
+
+@app.route('/products/', methods=['GET', 'POST'])
+def products():
+    if request.method == 'GET':
+        prods = {}
+        pb = []
+        print(Product.query.all())
+        for p in Product.query.all():
+            pb.append(p.serialize())
+        return jsonify({'products': pb}) 
+    if request.method == 'POST':
+        prod = Product(request.json.get('name',''), request.json.get('image',''), request.json.get('description',''), request.json.get('published',''))
+        db.session.add(prod)
+        db.session.commit()
+        return jsonify( { 'product': prod.serialize() }), 201
+
+@app.route('/products/<int:id>/')
+def product(id):
+    return jsonify({'product': Product.query.get(id).serialize()})
 
 @app.route('/')
 def index():
@@ -10,3 +77,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
