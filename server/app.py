d72b06b5e3539416e4234de5a06d6432cf0c6a34
Robin Sääf <robin.saaf@chas.se>
2017-04-07 10:19:45 +0200

    Resolve merge conflicts

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+<<<<<<< 5c6ec6b242626ed68beabf1f3a334978a624356c
 from flask import Flask, jsonify, abort, request
 from flask_sqlalchemy import SQLAlchemy
 import json
@@ -5,6 +6,41 @@ import json
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
+=======
+from flask import Flask, redirect, url_for, flash
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user,\
+     current_user
+from oauth import OAuthSignIn
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'top secret!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['OAUTH_CREDENTIALS'] = {
+    'linkedin': {
+        'id': '',
+        'secret': ''
+    }
+}
+
+db = SQLAlchemy(app)
+lm = LoginManager(app)
+lm.login_view = 'index'
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    social_id = db.Column(db.String(64), nullable=False, unique=True)
+    nickname = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=True)
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+>>>>>>> Add basic LinkedIn login functionality
 
 class User(db.Model):
     __tablename__='users'
@@ -90,6 +126,39 @@ def index():
     return "it worksss"
 
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/authorize/<provider>')
+def oauth_authorize(provider):
+    if not current_user.is_anonymous:
+        return redirect(url_for('index'))
+    oauth = OAuthSignIn.get_provider(provider)
+    return oauth.authorize()
+
+
+@app.route('/callback/<provider>')
+def oauth_callback(provider):
+    if not current_user.is_anonymous:
+        return redirect(url_for('index'))
+    oauth = OAuthSignIn.get_provider(provider)
+    social_id, username, email = oauth.callback()
+    if social_id is None:
+        flash('Authentication failed.')
+        return redirect(url_for('index'))
+    user = User.query.filter_by(social_id=social_id).first()
+    if not user:
+        user = User(social_id=social_id, nickname=username, email=email)
+        db.session.add(user)
+        db.session.commit()
+    login_user(user, True)
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
 
