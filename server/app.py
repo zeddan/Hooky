cd6a9a890ce7a0d184c84105f850ac11b2b33d96
Robin Sääf <robin.saaf@chas.se>
2017-04-07 11:26:06 +0200

    Merge

--- server/app.py
+++ server/app.py
@@ -1,58 +1,56 @@
-<<<<<<< 5c6ec6b242626ed68beabf1f3a334978a624356c
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-import json
+from oauth import OAuthSignIn
+from flask_login import LoginManager, UserMixin, login_user, logout_user,\
+     current_user
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-db = SQLAlchemy(app)
-=======
-from flask import Flask, redirect, url_for, flash
-
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, logout_user,\
-     current_user
-from oauth import OAuthSignIn
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'top secret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['OAUTH_CREDENTIALS'] = {
-    'linkedin': {
-        'id': '',
-        'secret': ''
-    }
-}
-
 db = SQLAlchemy(app)
 lm = LoginManager(app)
 lm.login_view = 'index'
 
 
-class User(UserMixin, db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    social_id = db.Column(db.String(64), nullable=False, unique=True)
-    nickname = db.Column(db.String(64), nullable=False)
-    email = db.Column(db.String(64), nullable=True)
-
-
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
->>>>>>> Add basic LinkedIn login functionality
 
-class User(db.Model):
-    __tablename__='users'
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
+    social_id = db.Column(db.String(64), unique=True)
+    name = db.Column(db.String(64))
     email = db.Column(db.String(80))
     admin = db.Column(db.Boolean)
 
-    def __init__(self, name, email, admin):
+    def __init__(self, name, email, social_id=None, admin=False):
+        self.social_id = social_id
         self.name = name
         self.email = email
         self.admin = admin
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'social_id': self.social_id,
+            'name': self.name,
+            'email': self.email,
+            'admin': self.admin
+        }
+
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -69,38 +67,42 @@ class Product(db.Model):
 
     def serialize(self):
         return {
-                'id':self.id,
-                'name':self.name,
-                'image':self.image,
-                'description':self.description,
-                'published':self.published,
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'description': self.description,
+            'published': self.published,
         }
 
-db.create_all()
 
 @app.route('/users/', methods=['GET', 'POST'])
 def users():
     if request.method == 'GET':
-        return jsonify({'users':User.query.all()})
+        pb = []
+        for p in User.query.all():
+            pb.append(p.serialize())
+        return jsonify({'users': pb})
+
 
 @app.route('/users/<int:id>/')
 def get_user(id):
-    return jsonify({'user':User.query.get(id)})
+    return jsonify({'user': User.query.get(id)})
+
 
 @app.route('/products/', methods=['GET', 'POST'])
 def products():
     if request.method == 'GET':
-        prods = {}
         pb = []
         print(Product.query.all())
         for p in Product.query.all():
             pb.append(p.serialize())
-        return jsonify({'products': pb}) 
+        return jsonify({'products': pb})
     if request.method == 'POST':
         prod = Product(request.json.get('name',''), request.json.get('image',''), request.json.get('description',''), request.json.get('published',''))
         db.session.add(prod)
         db.session.commit()
-        return jsonify( { 'product': prod.serialize() }), 201
+        return jsonify({'product': prod.serialize()}), 201
+
 
 @app.route('/products/<int:id>/', methods=['GET', 'DELETE', 'PUT'])
 def product(id):
@@ -110,7 +112,7 @@ def product(id):
     if request.method == 'DELETE':
         db.session.delete(Product.query.get(id))
         db.session.commit()
-        return jsonify( {'result' : True} )
+        return jsonify({'result': True})
 
     if request.method == 'PUT':
         prod = Product.query.get(id)
@@ -119,7 +121,8 @@ def product(id):
         prod.description = request.json.get('description', prod.description)
         prod.published = request.json.get('published', prod.published)
         db.session.commit()
-        return jsonify( {'product': prod.serialize() })
+        return jsonify({'product': prod.serialize()})
+
 
 @app.route('/')
 def index():
@@ -145,13 +148,13 @@ def oauth_callback(provider):
     if not current_user.is_anonymous:
         return redirect(url_for('index'))
     oauth = OAuthSignIn.get_provider(provider)
-    social_id, username, email = oauth.callback()
+    social_id, name, email = oauth.callback()
     if social_id is None:
         flash('Authentication failed.')
         return redirect(url_for('index'))
     user = User.query.filter_by(social_id=social_id).first()
     if not user:
-        user = User(social_id=social_id, nickname=username, email=email)
+        user = User(social_id=social_id, name=name, email=email)
         db.session.add(user)
         db.session.commit()
     login_user(user, True)
@@ -161,4 +164,3 @@ def oauth_callback(provider):
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)
-
--- server/config.py
+++ server/config.py
@@ -3,3 +3,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SECRET_KEY = ''
+OAUTH_CREDENTIALS = {
+    'linkedin': {
+        'id': '',
+        'secret': ''
+    }
+}
