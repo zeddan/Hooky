3e942430afbfc7325498374c7aa7305fc6a02bb3
Madeleine <maddepersoon@gmail.com>
2017-04-19 15:21:01 +0200

    change createClass to React.Component

--- application/src/app/Buttons.jsx
+++ application/src/app/Buttons.jsx
@@ -6,34 +6,16 @@ var {
 	Link
 } = ReactRouter;
 
-var LinkedinBtn = React.createClass( {
-
-		login() {
-			console.log("continue with linkedin");
-			fetch('http://localhost:5000/authorize/linkedin', { redirect: 'follow', mode: 'no-cors'})
-				.then((response) => { 
-					console.log(response);
-				});
-		},
-		render () {
-
+class LinkedinBtn extends React.Component {
+	render () {
 		return (
 			<a href="http://localhost:5000/authorize/linkedin">
 			<Button>Continue with LinkedIn</Button>
 			</a>
-		);
+		)
 	}
-});
+}
 
 
-const LoginBtn = React.createClass({
-	render: function() {
-		return (
-			<div>
-			<Button>Log in</Button>
-			</div>
-		);
-	}
-});
 
 export default LinkedinBtn;
