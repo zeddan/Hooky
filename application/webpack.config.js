0d5cd518e6a911b3f163208399c5e911edf7c6d2
Ola Dahl <oladahl.lel@gmail.com>
2017-04-19 15:29:16 +0200

    Uppdate config files

--- application/webpack.config.js
+++ application/webpack.config.js
@@ -1,19 +1,20 @@
+var webpack = require("webpack");
 var path = require("path");
 
-var SRC_DIR = path.resolve(__dirname, "src");
 var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
 
 var config = {
 	entry: SRC_DIR + "/app/index.jsx",
 	output: {
-		path: DIST_DIR + "/app/",
+		path: DIST_DIR + "/app",
 		filename: "bundle.js",
 		publicPath: "/app/"
 	},
 	module: {
 		loaders: [
 			{
-				test: /\.jsx?$/,
+				test: /\.jsx?/,
 				include: SRC_DIR,
 				loader: "babel-loader",
 				query: {
