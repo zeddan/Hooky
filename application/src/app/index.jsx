d1d9226070895cb4ecf336ae6fd802e4491f6d5e
Ola Dahl <oladahl.lel@gmail.com>
2017-04-19 15:29:58 +0200

    Update index files

--- application/src/app/index.jsx
+++ application/src/app/index.jsx
@@ -1,66 +1,26 @@
-import Home from './Home.jsx';
-import About from './About.jsx';
-import App from './App.jsx';
+import React from "react";
+import { render } from "react-dom";
+import {Router, Route, browserHistory, IndexRoute} from "react-router";
 
+import {Root} from "./components/Root.jsx";
+import {Home} from "./components/Home.jsx";
+import {About} from "./components/About.jsx";
 
-var { Router,
-    Route,
-    IndexRoute,
-    IndexLink,
-    hashHistory,
-    Link } = ReactRouter;
-
-var destination = document.querySelector("#container");
-
-
-var Stuff = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <h2>STUFF</h2>
-                <p>Mauris sem velit, vehicula eget sodales vitae,
-                    rhoncus eget sapien:</p>
-                <ol>
-                    <li>Nulla pulvinar diam</li>
-                    <li>Facilisis bibendum</li>
-                    <li>Vestibulum vulputate</li>
-                    <li>Eget erat</li>
-                    <li>Id porttitor</li>
-                </ol>
-            </div>
+class App extends React.Component {
+	render() {
+        return(
+          <Router history={browserHistory}>
+                <Route path="/" component={Root}>
+                    <IndexRoute component={Home}/>
+                    <Route path="about-us" component={About} />
+                </Route>
+           </Router>
         );
-    }
-});
+  }
+}
 
-var Map = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <h2>MAP</h2>
-
-                <ol>
-                    <li>Nulla pulvinar diam</li>
-                    <li>Facilisis bibendum</li>
-                    <li>Vestibulum vulputate</li>
-                    <li>Eget erat</li>
-                    <li>Id porttitor</li>
-                </ol>
-            </div>
-        );
-    }
-});
+render(<App/>, window.document.getElementById("app"));
 
 
 
 
-ReactDOM.render(
-    <Router history={hashHistory}>
-        <Route path="/" component={App}>
-            <IndexRoute component={Home}/>
-            <Route path="stuff" component={Stuff} />
-            <Route path="about-us" component={About} />
-            <Route path="map" component={Map} />
-        </Route>
-    </Router>,
-    destination
-);
