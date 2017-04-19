3336f19297c209e70fbf39760b442d2e96c6f31c
Robin Sääf <robin.saaf@chas.se>
2017-04-19 10:58:23 +0200

    Add Inspiration page

---
+++ application/src/app/Inspiration.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Inspiration extends React.Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default Inspiration;
