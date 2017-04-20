65d515aca425aef79069b6326c26a7655fffd5f7
Ola Dahl <oladahl.lel@gmail.com>
2017-04-20 17:42:07 +0200

    Update configuration files
    -Add css-loader and style-loader as devDependency.
    -Update webpack configuration to load css files.

--- application/webpack.config.js
+++ application/webpack.config.js
@@ -13,14 +13,19 @@ var config = {
 	},
 	module: {
 		loaders: [
-			{
-				test: /\.jsx?/,
-				include: SRC_DIR,
-				loader: "babel-loader",
-				query: {
-					presets: ["react", "es2015", "stage-2"]
-				}
+		{
+			test: /\.jsx?/,
+			include: SRC_DIR,
+			loader: "babel-loader",
+			query: {
+				presets: ["react", "es2015", "stage-2"]
 			}
+		},
+		{
+			test: /\.css$/,
+			include: SRC_DIR,
+			use: ['style-loader', 'css-loader']
+		}
 		]
 	}
 };
