7986aee89d484ed1f77780c83bec0141038e4f56
Robin Sääf <robin.saaf@chas.se>
2017-04-22 23:17:12 +0200

    Remove faulty prop declaration

--- application/src/app/components/pages/inspiration/GalleryModal.jsx
+++ application/src/app/components/pages/inspiration/GalleryModal.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class GalleryModal extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         if (this.props.isOpen === false) {
             return null;
         }
 
         return(
-            <div isOpen={this.props.isOpen} className='modal-overlay' onClick={this.props.onClick} name={this.props.name}>
+            <div className='modal-overlay' onClick={this.props.onClick} name={this.props.name}>
                 <div className='modal-body'>
                     <a className='modal-close' href='#' onClick={this.props.onClick}><span className='fa fa-times'></span></a>
                     <img src={this.props.src} />
