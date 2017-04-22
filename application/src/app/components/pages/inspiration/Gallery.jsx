c791777ea9c68accf35efc1a1970f22b3e0068b3
Robin Sääf <robin.saaf@chas.se>
2017-04-22 22:17:14 +0200

    Load products from server

--- application/src/app/components/pages/inspiration/Gallery.jsx
+++ application/src/app/components/pages/inspiration/Gallery.jsx
@@ -6,6 +6,7 @@ class Gallery extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            products: [],
             showModal: false,
             url: ''
         };
@@ -13,30 +14,38 @@ class Gallery extends React.Component {
         this.closeModal = this.closeModal.bind(this);
     };
 
+    componentDidMount() {
+        fetch('http://localhost:5000/products/').then((res) => {
+            return res.json();
+        }).then((json) => {
+            this.setState({ products: json.products });
+        });
+    };
+
     render() {
         const imgUrls = this.props.imgUrls;
         return(
-                <div className='container-fluid gallery-container'>
-                    <div className='row'>
-                        {
-                            imgUrls.map((url, index) => {
-                                return (
-                                    <div key={index + 1} className='col-sm-6 col-md-3 col-xl-2'>
-                                        <div className='gallery-card'>
-                                            <div className='like-heart'>
-                                                <div className='likes'>5</div>
-                                                <i className="heart fa fa-heart-o" aria-hidden="true"></i>
-                                            </div>
-                                            <GalleryImage className='gallery-thumbnail' src={url} alt={'Image number ' + (index + 1)} />
-                                            <span className='card-icon-open' value={url} onClick={(e) => this.openModal(url, e)}></span>
+            <div className='container-fluid gallery-container'>
+                <div className='row'>
+                    {
+                        this.state.products.map((product, index) => {
+                            return (
+                                <div key={index + 1} className='col-sm-6 col-md-3 col-xl-2'>
+                                    <div className='gallery-card'>
+                                        <div className='like-heart'>
+                                            <div className='likes'>{product.likes.length}</div>
+                                            <i className="heart fa fa-heart-o" aria-hidden="true"></i>
                                         </div>
+                                        <GalleryImage className='gallery-thumbnail' src={product.image} alt={'Image number ' + (index + 1)} />
+                                        <span className='card-icon-open' value={product.image} onClick={(e) => this.openModal(product.image, e)}></span>
                                     </div>
-                                );
-                            })
-                        }
-                    </div>
-                    <GalleryModal isOpen={this.state.showModal} onClick={this.closeModal} src={this.state.url} /> 
+                                </div>
+                            );
+                        })
+                    }
                 </div>
+                <GalleryModal isOpen={this.state.showModal} onClick={this.closeModal} src={this.state.url} /> 
+            </div>
         );
     };
 
