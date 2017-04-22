3c263837abe6373f9bd2dffdfda4e40d4633db8f
Robin Sääf <robin.saaf@chas.se>
2017-04-22 19:40:34 +0200

    Merge

--- application/src/app/App.jsx
+++ application/src/app/App.jsx
@@ -4,6 +4,8 @@ import {render} from "react-dom";
 //Components
 import Root from "./components/root/Root.jsx";
 import Home from "./components/pages/home/Home.jsx";
+import About from "./components/pages/home/About.jsx";
+import Inspiration from "./components/pages/inspiration/Inspiration.jsx";
 import {Router, Route, browserHistory, IndexRoute} from "react-router";
 
 class App extends React.Component {
@@ -12,6 +14,7 @@ class App extends React.Component {
           <Router history={browserHistory}>
                 <Route path="/" component={Root}>
                     <IndexRoute component={Home}/>
+                    <Route path="inspiration" component={Inspiration}/>
                 </Route>
            </Router>
         );
