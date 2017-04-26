88e813030ae932507db2f657dc2d286e9ec1d482
Robin Sääf <robin.saaf@chas.se>
2017-04-26 18:06:09 +0200

    Fix style according to PEP 8

--- server/app.py
+++ server/app.py
@@ -1,15 +1,15 @@
-from flask import Flask, jsonify, json, request, redirect, url_for, flash, send_from_directory
-from flask import Flask, jsonify, request, redirect, url_for, flash, abort
+from flask import Flask, jsonify, request, redirect, url_for, flash, abort, \
+    json, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from oauth import OAuthSignIn
-from flask_login import LoginManager, UserMixin, login_user, logout_user,\
-        current_user
+from flask_login import LoginManager, UserMixin, login_user, logout_user, \
+    current_user
 from werkzeug.security import generate_password_hash, \
-        check_password_hash
-from flask_cors import CORS, cross_origin
+    check_password_hash
+from flask_cors import CORS
 from werkzeug.utils import secure_filename
-import os, time
-import requests
+import os
+import time
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -20,14 +20,17 @@ CORS(app)
 
 url = 'http://localhost:5000'
 
-@lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 
-likes = db.Table('likes',
-                 db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
-                 db.Column('product_id', db.Integer, db.ForeignKey('product.id'))
-                )
+@lm.user_loader
+def load_user(u_id):
+    return User.query.get(int(u_id))
+
+
+users_products = db.Table('likes',
+                          db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+                          db.Column('product_id', db.Integer, db.ForeignKey('product.id'))
+                          )
+
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -39,10 +42,9 @@ class User(UserMixin, db.Model):
     pw_hash = db.Column(db.String(200))
     products = db.relationship(
         'Product',
-        secondary=likes,
+        secondary=users_products,
         lazy='joined',
         back_populates="users")
-
 
     def __init__(self, name, email, admin=False, social_id=None):
         self.social_id = social_id
@@ -84,10 +86,9 @@ class Product(db.Model):
     published = db.Column(db.Boolean, default=False)
     users = db.relationship(
         'User',
-        secondary=likes,
+        secondary=users_products,
         lazy='joined',
         back_populates="products")
-
 
     def __init__(self, user_id, name, image, description, published=False):
         self.user_id = user_id
@@ -123,51 +124,55 @@ def like():
     return jsonify({'product': product}), 201
 
 
-@app.route('/users/', methods=['GET', 'POST'])
-def users():
-    if request.method == 'GET':
-        users = []
-        for p in User.query.all():
-            users.append(p.serialize())
-        return jsonify({'users': users})
+@app.route('/users/', methods=['GET'])
+def get_users():
+    users = []
+    for p in User.query.all():
+        users.append(p.serialize())
+    return jsonify({'users': users})
 
 
-@app.route('/users/<int:id>/')
-def get_user(id):
-    return jsonify({'user': User.query.get(id)})
+@app.route('/users/<int:u_id>/')
+def get_user(u_id):
+    return jsonify({'user': User.query.get(u_id)})
+
 
 def append_time(filename):
     name, ext = os.path.splitext(filename)
     return "{name}_{time}{ext}".format(name=name, time=time.strftime("%Y%m%d-%H%M%S"), ext=ext)
+
 
 def get_image_url(files):
     # check if the post request has the file part
     if 'file' not in files:
         flash('No file part')
         return ''
-    file = files['file']
+    image = files['file']
     # if user does not select file, browser also
     # submit a empty part without filename
-    if file.filename == '':
+    if image.filename == '':
         flash('No selected file')
         return ''
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
+    if image and allowed_file(image.filename):
+        filename = secure_filename(image.filename)
         filename = append_time(filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         print(filename)
-        return url + url_for('uploaded_file',filename=filename)
+        return url + url_for('uploaded_file', filename=filename)
+
 
 def allowed_file(filename):
     return '.' in filename and \
-            filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
 
 @app.route('/images/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 
 @app.route('/products/', methods=['GET', 'POST'])
-def products():
+def get_post_products():
     if request.method == 'GET':
         products = []
         for p in Product.query.all():
@@ -179,17 +184,17 @@ def products():
         return jsonify({'products': products})
     if request.method == 'POST':
         image = get_image_url(request.files)
-        prod = Product(1,request.form['name'], image,
+        prod = Product(1, request.form['name'], image,
                        request.form['description'])
         db.session.add(prod)
         db.session.commit()
         return jsonify({'product': prod.serialize()}), 201
 
 
-@app.route('/products/<int:id>/', methods=['GET', 'DELETE', 'PUT'])
-def product(id):
+@app.route('/products/<p_id>/', methods=['GET', 'DELETE', 'PUT'])
+def get_del_put_product(p_id):
     if request.method == 'GET':
-        product = Product.query.get(id)
+        product = Product.query.get(p_id)
         likes = []
         [likes.append(u.id) for u in product.users]
         product = product.serialize()
@@ -197,15 +202,15 @@ def product(id):
         return jsonify({'product': product})
 
     if request.method == 'DELETE':
-        db.session.delete(Product.query.get(id))
+        db.session.delete(Product.query.get(p_id))
         db.session.commit()
         return jsonify({'result': True})
 
     if request.method == 'PUT':
         data = json.loads(request.data)
         user = User.query.get(data['user_id'])
-        if user.admin == True:
-            prod = Product.query.get(id)
+        if user.admin:
+            prod = Product.query.get(p_id)
             prod.name = data['name']
             prod.image = data['image']
             prod.description = data['description']
@@ -226,6 +231,7 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
 @app.route('/register', methods=['POST'])
 def register():
     data = json.loads(request.data)
@@ -234,13 +240,14 @@ def register():
     name = data['name']
     if email is None or password is None:
         abort(400)
-    if User.query.filter_by(email = email).first() is not None:
+    if User.query.filter_by(email=email).first() is not None:
         abort(403)
     user = User(name, email)
     user.set_password(password)
     db.session.add(user)
     db.session.commit()
     return redirect(url_for('index'))
+
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -257,6 +264,7 @@ def login():
         return redirect('http://localhost:8080')
     flash('Bad password')
     return redirect(url_for('index'))
+
 
 @app.route('/authorize/<provider>')
 def oauth_authorize(provider):
