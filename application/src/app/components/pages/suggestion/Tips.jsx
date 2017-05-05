c7220d50f99a027b232ed8af703da73de6812fac
Ola Dahl <oladahl.lel@gmail.com>
2017-05-05 23:40:30 +0200

    Display picked image in image box

--- application/src/app/components/pages/suggestion/Tips.jsx
+++ application/src/app/components/pages/suggestion/Tips.jsx
@@ -22,7 +22,7 @@ import {
 				supplierEmail: '',
 				supplierAddress: '',
 				file: '',
-				imageURL: ''
+				imageURL: 'https://itavo.nl/img/placeholder.jpg',
 			};
 
 			this.handleChange = this.handleChange.bind(this);
@@ -43,21 +43,38 @@ import {
 			});
 		}
 
+		_handleImageChange(e) {
+			e.preventDefault();
+
+			let reader = new FileReader();
+			let file = e.target.files[0];
+
+			reader.onloadend = () => {
+
+				this.setState({
+					file: file,
+					imageURL: reader.result
+				});
+			}
+
+			reader.readAsDataURL(file)
+		}
+
 		render() {
 			return (
 
 				<Grid id="content-container" className="show-grid">
 					<Form onSubmit={this.handleSubmit}>
-					<Row>
-						<Col lg={5} md={4} sm={6} xs={6} id="left-col">
-							<div className="items-container" >
-								<div className="image-container">
-									<Thumbnail src="https://www.highmowingseeds.com/pub/media/catalog/product/cache/1/image/675x675/e9c3970ab036de70892d86c6d221abfe/2/6/2686.jpg">
-										<h3>Produktbild</h3>
-										<p>Lägg till en bild på produkten som fångar intresse.</p>
+						<Row className="top-row">
+							<Col lg={5} md={4} sm={6} xs={6} id="left-col">
+								<div className="items-container" >
+									<div className="image-container">
+										<Thumbnail src={this.state.imageURL}>
+											<h3>Produktbild</h3>
+											<p>Lägg till en bild på produkten som fångar intresse.</p>
 
-										<input type="file" name="file" id="file" className="inputfile" />
-										<label htmlFor="file">Lägg till bild</label>
+											<input type="file" name="file" id="file" className="inputfile" onChange={(e)=>this._handleImageChange(e)}/>
+											<label htmlFor="file" className="btn btn-default">Lägg till bild</label>
 										</Thumbnail>
 									</div>
 								</div>
@@ -66,100 +83,100 @@ import {
 							<Col lg={7} md={7} sm={6} xs={6} id="right-col">
 								<h3>Produkt</h3>
 
-									<FormGroup>
-										<ControlLabel>Namn på produkt</ControlLabel>
-										<InputGroup>
-											<InputGroup.Addon><i className='fa fa-info-circle'/></InputGroup.Addon>
-											<FormControl
-												name='name'
-												type='text'
-												placeholder='Produktnamn'
-												onChange={this.handleChange}/>
-										</InputGroup>
-									</FormGroup>
-
-									<FormGroup>
-										<ControlLabel>Beskrivning av produkt</ControlLabel>
+								<FormGroup>
+									<ControlLabel>Namn på produkt</ControlLabel>
+									<InputGroup>
+										<InputGroup.Addon><i className='fa fa-info-circle'/></InputGroup.Addon>
 										<FormControl
-											name='description'
-											componentClass='textarea'
-											placeholder='Beskrivning...'
-											onChange={this.handleChange} />
-									</FormGroup>
+											name='name'
+											type='text'
+											placeholder='Produktnamn'
+											onChange={this.handleChange}/>
+									</InputGroup>
+								</FormGroup>
 
-									<h3>Leverantör</h3>
+								<FormGroup>
+									<ControlLabel>Beskrivning av produkt</ControlLabel>
+									<FormControl
+										name='description'
+										componentClass='textarea'
+										placeholder='Beskrivning...'
+										onChange={this.handleChange} />
+								</FormGroup>
 
-									<FormGroup>
-										<ControlLabel>Namn</ControlLabel>
-										<InputGroup>
-											<InputGroup.Addon><i className='fa fa-truck'/></InputGroup.Addon>
-											<FormControl
-												name='supplierName'
-												type='text'
-												placeholder='Leverantörens namn'
-												onChange={this.handleChange}/>
-										</InputGroup>
-									</FormGroup>
+								<h3>Leverantör</h3>
 
-									<FormGroup>
-										<ControlLabel>Hemsida</ControlLabel>
-										<InputGroup>
-											<InputGroup.Addon><i className='fa fa-globe'/></InputGroup.Addon>
-											<FormControl
-												name='supplierWebpage'
-												type='text'
-												placeholder='Hemsida'
-												onChange={this.handleChange}/>
-										</InputGroup>
-									</FormGroup>
+								<FormGroup>
+									<ControlLabel>Namn</ControlLabel>
+									<InputGroup>
+										<InputGroup.Addon><i className='fa fa-truck'/></InputGroup.Addon>
+										<FormControl
+											name='supplierName'
+											type='text'
+											placeholder='Leverantörens namn'
+											onChange={this.handleChange}/>
+									</InputGroup>
+								</FormGroup>
 
-									<FormGroup>
-										<ControlLabel>Telefonnummer</ControlLabel>
-										<InputGroup>
-											<InputGroup.Addon><i className='fa fa-phone'/></InputGroup.Addon>
-											<FormControl
-												name='supplierPhone'
-												type='text'
-												placeholder='Nummer'
-												onChange={this.handleChange}/>
-										</InputGroup>
-									</FormGroup>
+								<FormGroup>
+									<ControlLabel>Hemsida</ControlLabel>
+									<InputGroup>
+										<InputGroup.Addon><i className='fa fa-globe'/></InputGroup.Addon>
+										<FormControl
+											name='supplierWebpage'
+											type='text'
+											placeholder='Hemsida'
+											onChange={this.handleChange}/>
+									</InputGroup>
+								</FormGroup>
 
-									<FormGroup>
-										<ControlLabel>Email</ControlLabel>
-										<InputGroup>
-											<InputGroup.Addon><i className='fa fa-envelope'/></InputGroup.Addon>
-											<FormControl
-												name='supplierEmail'
-												type='text'
-												placeholder='Email'
-												onChange={this.handleChange}/>
-										</InputGroup>
-									</FormGroup>
+								<FormGroup>
+									<ControlLabel>Telefonnummer</ControlLabel>
+									<InputGroup>
+										<InputGroup.Addon><i className='fa fa-phone'/></InputGroup.Addon>
+										<FormControl
+											name='supplierPhone'
+											type='text'
+											placeholder='Nummer'
+											onChange={this.handleChange}/>
+									</InputGroup>
+								</FormGroup>
 
-									<FormGroup>
-										<ControlLabel>Adress</ControlLabel>
-										<InputGroup>
-											<InputGroup.Addon><i className='fa fa-address-card'/></InputGroup.Addon>
-											<FormControl
-												name='supplierAddress'
-												type='text'
-												placeholder='Adress'
-												onChange={this.handleChange}/>
-										</InputGroup>
-									</FormGroup>
+								<FormGroup>
+									<ControlLabel>Email</ControlLabel>
+									<InputGroup>
+										<InputGroup.Addon><i className='fa fa-envelope'/></InputGroup.Addon>
+										<FormControl
+											name='supplierEmail'
+											type='text'
+											placeholder='Email'
+											onChange={this.handleChange}/>
+									</InputGroup>
+								</FormGroup>
 
-								</Col>
-							</Row>
-							<Row>
-								<Col>
-									<Button id='submit-btn' bsStyle='default' bsSize='large' type='submit' block>Lägg till produkt</Button>
-								</Col>
-							</Row>
-						</Form>
-					</Grid>
-				);
-			}
+								<FormGroup>
+									<ControlLabel>Adress</ControlLabel>
+									<InputGroup>
+										<InputGroup.Addon><i className='fa fa-address-card'/></InputGroup.Addon>
+										<FormControl
+											name='supplierAddress'
+											type='text'
+											placeholder='Adress'
+											onChange={this.handleChange}/>
+									</InputGroup>
+								</FormGroup>
+
+							</Col>
+						</Row>
+						<Row>
+							<Col>
+								<Button id='submit-btn' bsStyle='default' bsSize='large' type='submit' block>Lägg till produkt</Button>
+							</Col>
+						</Row>
+					</Form>
+				</Grid>
+			);
 		}
+	}
 
-		export default Tips;
+	export default Tips;
