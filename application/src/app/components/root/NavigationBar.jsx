6aa99edee67b9e601eafb3f4155e0bc18ea9104a
Robin Sääf <robin.saaf@chas.se>
2017-05-19 11:29:25 +0200

    Remove dead code in NavigationBar.jsx

--- application/src/app/components/root/NavigationBar.jsx
+++ application/src/app/components/root/NavigationBar.jsx
@@ -13,7 +13,6 @@ import UserMenu from './UserMenu.jsx';
 import '../../css/style.scss';
 
 class NavigationBar extends React.Component {
-// <<<<<<< Updated upstream
 
     constructor() {
         super();
@@ -23,6 +22,14 @@ class NavigationBar extends React.Component {
             email: 'info@djakne.se',
             showModal: false
         }
+    }
+
+    componentWillMount() {
+        setTimeout(() => {
+            this.setState({
+                name: Cookies.get('name')
+            });
+        }, 750);
     }
 
     componentDidMount() {
@@ -39,11 +46,6 @@ class NavigationBar extends React.Component {
             }
         });
 
-        setTimeout(() => {
-            this.setState({
-                name: Cookies.get('name')
-            });
-        }, 100);
     };
 
     toggleMenu() {
@@ -70,8 +72,6 @@ class NavigationBar extends React.Component {
         this.setState({
             isVisible: false
         });
-        {/*<<<<<<< Updated upstream*/
-        }
         var path = `/admin`;
         browserHistory.push(path);
     }
@@ -136,107 +136,6 @@ class NavigationBar extends React.Component {
             </div>
         );
     }
-
-// =======
-//     }, 100);
-//   }
-//
-//   render() {
-//     if (this.state.name == '') {
-//       return null;
-//     }
-//     return(
-//       <Navbar fixedTop={true} >
-//         <Nav>
-//           <LinkContainer to={"/inspiration"} className="hidden-md hidden-lg">
-//             <NavItem>
-//               <i className="fa fa-home fa-lg"/>
-//             </NavItem>
-//           </LinkContainer>
-//
-//           <LinkContainer to={"/inspiration"} className="hidden-sm hidden-xs">
-//             <NavItem id="nav-item-home">
-//               Hooky
-//             </NavItem>
-//           </LinkContainer>
-//
-//           <LinkContainer to={"/suggestion"} className="hidden-md hidden-lg">
-//             <NavItem>
-//               <i className="fa fa-plus fa-lg" />
-//             </NavItem>
-//           </LinkContainer>
-//
-//           <LinkContainer to={"/account"} className="hidden-md hidden-lg">
-//             <NavItem>
-//               <i className="fa fa-user-circle fa-lg"/>
-//             </NavItem>
-//           </LinkContainer>
-//
-//         </Nav>
-//         <Nav className="hidden-sm hidden-xs" pullRight>
-//           <LinkContainer to={"/suggestion"}>
-//             <NavItem id="nav-item-tip">
-//               Tipsa!
-//             </NavItem>
-//           </LinkContainer>
-//
-//           <LinkContainer to={"/account"}>
-//             <NavItem id="nav-item-profile">
-//               {this.state.name}
-//             </NavItem>
-//           </LinkContainer>
-//         </Nav>
-//       </Navbar>
-//     );
-//   }
-// =======
-//     render() {
-//         return (
-//             <Navbar fixedTop={true}>
-//                 <Nav>
-//                     <LinkContainer to={"/inspiration"} className="hidden-md hidden-lg">
-//                         <NavItem>
-//                             <i className="fa fa-home fa-lg"/>
-//                         </NavItem>
-//                     </LinkContainer>
-//
-//                     <LinkContainer to={"/inspiration"} className="hidden-sm hidden-xs">
-//                         <NavItem id="nav-item-home">
-//                             Hooky
-//                         </NavItem>
-//                     </LinkContainer>
-//
-//                     <LinkContainer to={"/suggestion"} className="hidden-md hidden-lg">
-//                         <NavItem>
-//                             <i className="fa fa-plus fa-lg"/>
-//                         </NavItem>
-//                     </LinkContainer>
-//
-//                     <LinkContainer to={"/account"} className="hidden-md hidden-lg">
-//                         <NavItem>
-//                             <i className="fa fa-user-circle fa-lg"/>
-//                         </NavItem>
-//                     </LinkContainer>
-//
-//                 </Nav>
-//                 <Nav className="hidden-sm hidden-xs" pullRight>
-//                     <LinkContainer to={"/suggestion"}>
-//                         <NavItem id="nav-item-tip">
-//                             Tipsa!
-//                         </NavItem>
-//                     </LinkContainer>
-//
-//                     <LinkContainer to={"/account"}>
-//                         <NavItem id="nav-item-profile">
-//                             Företaget
-//                         </NavItem>
-//                     </LinkContainer>
-//                 </Nav>
-//             </Navbar>
-//         );
-//     }
-// >>>>>>> Stashed changes
-// >>>>>>> Stashed changes
 
 }
 export default NavigationBar;
