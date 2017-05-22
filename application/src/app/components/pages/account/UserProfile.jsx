fef60ce20c6d519149fa9cce819f36a8e1309e98
Madeleine <maddepersoon@gmail.com>
2017-05-22 15:34:02 +0200

    add info to user profile

--- application/src/app/components/pages/account/UserProfile.jsx
+++ application/src/app/components/pages/account/UserProfile.jsx
@@ -1,65 +1,129 @@
 import React from 'react';
 
-import {Grid, Row, Col, Form, FormGroup} from 'react-bootstrap';
+import {Grid, Row, HelpBlock, Col, Form, FormGroup, ControlLabel, InputGroup, FormControl, Button} from 'react-bootstrap';
+import '../../../css/style.scss';
 import Cookies from 'js-cookie';
+import Popup from '../../Popup.jsx';
 
-//import '../../../css/account.scss';
+import '../../../css/_account.scss';
 
-class UserProfile extends React.Component {
+class Account extends React.Component {
    constructor(props) {
       super(props);
       this.state = {
-         name: 'Djäkne',
+         name: '',
          email: 'info@djakne.se',
-	 company: 'Okänt'
+	 about: '',
+	 zipcode: '12345',
+	 company: 'Ditt företag',
+	 delivers_to:'Vilket område är du verksam i',
+	 id: '',
+	 linkedin: true,
+	 showPopup: false
       };
       this.logout = this.logout.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.openPopup = this.openPopup.bind(this);
    };
 
+   handleChange(event) {
+      this.setState({
+         [event.target.name]: event.target.value,
+      });
+   }
+
+   handleSubmit(event) {
+            event.preventDefault();
+                fetch('http://localhost:5000/users/' + this.state.id + '/', {
+                    method: 'PUT',
+		    credentials: 'include',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        name: this.state.name,
+                        email: this.state.email,
+                        about: this.state.about,
+                        zipcode: this.state.zipcode,
+                        company: this.state.company,
+                        delivers_to: this.state.delivers_to,
+                    })
+                }).then((response) => {
+			this.openPopup();
+		});
+    }
+
+   openPopup() {
+        this.setState({showPopup: true});
+    }
+
+    closePopup() {
+        this.setState({showPopup: false});
+    }
+
    componentDidMount() {
-      fetch('http://localhost:5000/users/'+this.props.routeParams.userID, {credentials: 'include'}).then((res) => {
+      fetch('http://localhost:5000/users/'+this.props.routeParams.userID+'/').then((res) => {
          return res.json();
       }).then((json) => {
          console.log(json.user);
          if (json.user.email != undefined) {
             this.setState({
-		name: json.user.name, 
-		email: json.user.email, 
-		company: json.user.company,
-	    	delivers_to: json.user.delivers_to
+		    name: json.user.name,
+		    email: json.user.email,
+		    about: (json.user.about != null) ? json.user.about : '',
+		    zipcode: (json.user.zipcode != null) ? json.user.zipcode : '',
+		    company: (json.user.company != null) ? json.user.company : '',
+		    delivers_to: (json.user.delivers_to != null) ? json.user.delivers_to : '',
+		    id: json.user.id,
+		    linkedin: (json.user.social_id == null) ? true : false
 	    });
-         }
-      });
-   };
+	 }
+	 });
+   }
+
 
    render() {
       return (
+	<div>
+              <Popup show={this.state.showPopup}
+                     close={e => this.closePopup(e)}
+                     message="Uppgifterna ändrade"
+                     />
          <Grid>
             <Row>
                <Col xs={12}>
-                  <Form>
-                     <h2>{this.state.name}</h2>
+	      	  <Form onSubmit={this.handleSubmit}>
+		  <h2>{this.state.name}</h2>
 
-                     <div className="profile-item">
-                        <h3>Email</h3>
-                        <h4>{this.state.email}</h4>
-                     </div>
+			
+	      			<p>{this.state.email}</p>
 
-		     <div className="profile-item">
-                        <h3>Företag</h3>
-                        <h4>{this.state.company}</h4>
-                     </div>
+                     <FormGroup>
+                        <ControlLabel>Presentation</ControlLabel>
+			<p>{this.state.about}</p>
+                     </FormGroup>
 
-	      	     <div className="profile-item">
-	                <h3>Verksam i</h3>
-	                <h4>{this.state.delivers_to}</h4>
-	             </div>
+                     <FormGroup>
+                        <ControlLabel>Företag</ControlLabel>
+			<p>{this.state.company}</p>
+                     </FormGroup>
 
+                     <FormGroup>
+                        <ControlLabel>Verksam i</ControlLabel>
+			<p>{this.state.delivers_to}</p>
+                     </FormGroup>
+
+                     <FormGroup>
+                        <ControlLabel>Postnummer</ControlLabel>
+			<p>{this.state.zipcode}</p>
+                     </FormGroup>
                   </Form>
-
                </Col>
             </Row>
          </Grid>
+	</div>
       )
    }
 
@@ -74,4 +138,4 @@ class UserProfile extends React.Component {
    }
 }
 
-export default UserProfile;
+export default Account;
